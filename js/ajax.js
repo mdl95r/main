75579df0089af8cf3b71d7f953849ca3383d242c
Artyom Bogomolov <awesomemdl95@gmail.com>
2020-05-12 19:42:45 +0300

    Update files

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$("form").submit(function(event) {
+	$(".form-top").submit(function(event) {
     try {
       yaCounter60749653.reachGoal('formSubmit'); 
     } catch (e) {
@@ -16,13 +16,46 @@ $(document).ready(function() {
       var $method = $(this).attr("method"); 
     }
     
+    const $url = "/handler.php"
+    
     $.ajax({
       url: $url,
       type: $method,
       data: $data, 
       dataType: "json",
       complete: function(){ 
-        $('form')[0].reset();
+        $('.form-top')[0].reset();
+      }
+    });
+  });
+  
+  
+  $(".form-bottom").submit(function(event) {
+    try {
+        yaCounter60749653.reachGoal('fastMaster');
+    } catch (e) {
+        console.log('Отправка данных не удалась', e); 
+    }
+  
+    event.preventDefault();
+    $data2 = $(this).serialize();
+
+    if(!$(this).attr("method")) { 
+      var $method2 = "post"; 
+    } else { 
+      var $method2 = $(this).attr("method"); 
+    }
+    
+    const $url2 = "/handler2.php"
+    
+    $.ajax({
+      url: $url2,
+      type: $method2,
+      data: $data2, 
+      dataType: "json",
+      complete: function(){
+        $(".popup-main").slideUp(200);
+        $('.form-bottom')[0].reset();
       }
     });
   });
